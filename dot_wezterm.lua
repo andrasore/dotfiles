950c34336a029aa48c3cd2eba658cff0f3209a2a
Andras Sore <andras08@gmail.com>
2022-02-12 15:07:41 +0100

    add wezterm, inputrc. update xfce4

---
+++ dot_wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm';
+
+return {
+	color_scheme = "Urple",
+    font = wezterm.font("Terminus"),
+    font_size = 12,
+    use_fancy_tab_bar = false,
+    window_padding = {
+        left = 8,
+        right = 8,
+        bottom = 2,
+        up = 8,
+    }
+}
