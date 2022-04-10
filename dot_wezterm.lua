104c59401a01335b3c3183f7655f7b2c37576c7b
Andras Sore <andras08@gmail.com>
2022-04-10 20:24:28 +0200

    update wezterm config

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -2,13 +2,15 @@ local wezterm = require 'wezterm';
 
 return {
 	color_scheme = "Urple",
-    font = wezterm.font("Terminus"),
-    font_size = 12,
+    font = wezterm.font("Iosevka"),
+    font_size = 10,
     use_fancy_tab_bar = false,
     window_padding = {
         left = 8,
         right = 8,
         bottom = 2,
         up = 8,
-    }
+    },
+    initial_rows = 40,
+    initial_cols = 160
 }
