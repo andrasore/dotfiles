33b56fb0b2d8a7c265d497df260c7c03d6aecfa4
Andras Sore <andras08@gmail.com>
2022-08-15 08:24:37 +0200

    add fonts

---
+++ dot_fonts/iosevka-custom/executable_command.sh
@@ -0,0 +1,2 @@
+make custom-config italic='cv04 cv08 cv24' upright='cv03 cv07 cv12' design='cv01 cv13 cv17 cv19 cv21 cv22 cv26 cv30 cv31 cv35 cv37 cv38 cv40 cv43 cv45 cv46' weights='extralight light book medium bold'
+ 
