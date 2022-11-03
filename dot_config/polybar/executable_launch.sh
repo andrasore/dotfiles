5b8d51eaa7d04291e3aeb08be49743d59949d61b
Andras Sore <andras08@gmail.com>
2022-11-03 21:20:02 +0100

    Fix polybar lauch script

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -13,15 +13,15 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
 
 if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -l) == 1 ]]; then
-  MONITOR=$(polybar --list-monitors | cut -d":" -f1) TRAY_POS=right polybar main &
+  MONITOR=$(polybar --list-monitors | cut -d":" -f1) TRAY_POS=right polybar top &
 else
   primary=$(xrandr --query | grep primary | cut -d" " -f1)
 
   for m in $screens; do
     if [[ $primary == $m ]]; then
-        MONITOR=$m TRAY_POS=right polybar -q top -c "$DIR"/config.ini &
+        MONITOR=$m TRAY_POS=right polybar top -c "$DIR"/config.ini &
     else
-        MONITOR=$m TRAY_POS=none polybar -q secondary -c "$DIR"/config.ini &
+        MONITOR=$m TRAY_POS=none polybar secondary -c "$DIR"/config.ini &
     fi
   done
 fi
